6066ef677d6d2542475c824e8a045220ea22e2f3
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-03-02 12:44:49 +0000

    running in loop and removed old test code

--- einkwho.py
+++ einkwho.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # This is for testing without a raspberry pi or eink display, it will create and image and open it in preview
 
+import time
 from PIL import Image, ImageFont, ImageDraw
 from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
 from pktools import pktools
@@ -26,21 +27,47 @@ img  = Image.new( mode = "P", size = inky_display.resolution )
 # Create a varible that allows access to the drawing functions
 draw = ImageDraw.Draw(img)
 
-# Checkthe data is up to date
+# Create and image to draw on the scren
+def drawScreen():
+    # Get the first member in the last switch
+    firstFront = pktools.getMember(pktools.lastSwitch["members"][0])
+
+    # Draw a white background on the display
+    draw.rectangle(((0, 0), inky_display.resolution), inky_display.WHITE, None, 0)
+
+    # Draw text on the display
+    draw.text((inky_display.resolution[0] / 2, 32), firstFront["name"], inky_display.BLACK, font=bigFont, anchor="mm")
+    draw.text((8, 86), firstFront["pronouns"], inky_display.BLACK, font=smallFont, anchor="lm")
+
+    if firstFront["uuid"] in flagGroup["members"]:
+        draw.text((inky_display.resolution[0] - 8, 86), "ty", inky_display.RED, font=smallFont, anchor="rm")
+
+    return(img)
+
+
+# Checkthe data is up to date on first run
 pktools.pullPeriodic()
-
-# Get the first member in the last switch
-firstFront = pktools.getMember(pktools.lastSwitch["members"][0])
-
-# Draw a white background on the display
-draw.rectangle(((0, 0), inky_display.resolution), inky_display.WHITE, None, 0)
-
-# Draw text on the display
-draw.text((inky_display.resolution[0] / 2, 32), firstFront["name"], inky_display.BLACK, font=bigFont, anchor="mm")
-draw.text((8, 86), firstFront["pronouns"], inky_display.BLACK, font=smallFont, anchor="lm")
-
-if firstFront["uuid"] in flagGroup["members"]:
-    draw.text((inky_display.resolution[0] - 8, 86), "ty", inky_display.RED, font=smallFont, anchor="rm")
-
-inky_display.set_image(img)
+inky_display.set_image(drawScreen())
 inky_display.show()
+
+### Main code loop ###
+minutePast = 0
+
+while True:
+    # Don't do anyting if the minute hasn't changed
+    if minutePast != time.localtime()[4]:
+        minutePast = time.localtime()[4]
+
+        # If the minute is divisable by five check for new fronters
+        # this is for rate limiting and not hitting the pluralkit api too hard
+        if ( time.localtime()[4] % 5 ) == 0:
+            updateNeeded = pktools.pullPeriodic()
+
+            # If pullPeriodic returns true update the screen and unset updateNeeded
+            if updateNeeded:
+                inky_display.set_image(drawScreen())
+                inky_display.show()
+                updateNeeded = False
+
+    # do nothing for a while
+    time.sleep(30)
