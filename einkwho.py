0ad7a9e1dce26c3f086580b12cd80a9bf7d06b8b
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-06-15 14:09:09 +0100

    Removing redundant code, added logging and config

--- einkwho.py
+++ einkwho.py
@@ -2,14 +2,25 @@
 # This is for testing without a raspberry pi or eink display, it will create and image and open it in preview
 
 import time
-import requests
+import yaml
+import json
 import logging
 from PIL import Image, ImageFont, ImageDraw
-from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
 from pktools import pktools
 from inky.auto import auto
 
-# Check that the libary is up to date enough
+# Logging setup
+logging.basicConfig(format="%(asctime)s : %(message)s", filename="einkwhois.log", encoding='utf-8', level=logging.INFO)
+
+# Load settings
+try:
+    with open("./config-einkwhois.yaml", "r") as read_file:
+        config = yaml.safe_load(read_file)
+except:
+    logging.critical("Settings file missing")
+    exit()
+
+# Check that the library is up to date enough
 try:
     inky_display = auto(ask_user=True, verbose=True)
 except TypeError:
@@ -23,27 +34,21 @@ flagGroup = [i for i in pktools.pkGroups if i["id"] == flagGroupId][0]
 bigFont = ImageFont.truetype("./LeagueSpartan-Medium.ttf", int(44))
 smallFont = ImageFont.truetype("./LeagueSpartan-Medium.ttf", int(24))
 
-# Create a varible that holds what will be drawn onto the screen
+# Create a variable that holds what will be drawn onto the screen
 img  = Image.new( mode = "P", size = inky_display.resolution )
 
-# Create a varible that allows access to the drawing functions
+# Create a variable that allows access to the drawing functions
 draw = ImageDraw.Draw(img)
 
-# Checks if a system member is publicly visible and if not returns the default member
-def testPrivacy(id):
-    if pktools.getMember(id)["privacy"]["visibility"] == "private":
-        return pktools.pktsettings["defaultFronter"]
-    else: 
-        return id
 
 # Returns the member that should be displayed on the display
 def getFronter():
-    if len(pktools.lastSwitch["members"]) > 0:
-        return pktools.getMember(testPrivacy(pktools.lastSwitch["members"][0]))
-    else:
-        return pktools.getMember(pktools.pktsettings["defaultFronter"])
+    a = 1
 
-# Create and image to draw on the scren
+def fetchState():
+    b=1
+
+# Create and image to draw on the screen
 def drawScreen(fronter):
     # Draw a white background on the display
     draw.rectangle(((0, 0), inky_display.resolution), inky_display.WHITE, None, 0)
@@ -57,21 +62,12 @@ def drawScreen(fronter):
     if fronter["uuid"] in flagGroup["members"]:
         draw.text((inky_display.resolution[0] - 8, 86), "ty", inky_display.RED, font=smallFont, anchor="rm")
 
-    # Rotate the image as the pi has power cables comming out the usb ports so is mounted gpio connector down
+    # Rotate the image as the pi has power cables coming out the usb ports so is mounted gpio connector down
     return(img.rotate(180))
 
-# Send a message to discord saying who's fronting
-def sendMessage(messageText, mode):
-    logging.info("Sending Discord message")
-    message = {"content": messageText}
-    try:
-        requests.post("https://discord.com/api/webhooks/" + pktools.pktsettings["discord"][mode]["serverID"] + "/" + pktools.pktsettings["discord"][mode]["token"], message)
-    except requests.exceptions.RequestException as e:
-        logging.warning("Unable to send message to discord")
-        logging.warning(e) 
 
-# Checkthe data is up to date on first run
-pktools.pullPeriodic()
+# Check the data is up to date on first run
+fetchState()
 inky_display.set_image(drawScreen(getFronter()))
 inky_display.show()
 
@@ -79,64 +75,20 @@ inky_display.show()
 minutePast = 0
 
 while True:
-    # Don't do anyting if the minute hasn't changed
+    # Don't do anything if the minute hasn't changed
     if minutePast != time.localtime()[4]:
         minutePast = time.localtime()[4]
 
-        # If the minute is divisable by updateInterval check for new fronters
+        # If the minute is divisible by updateInterval check for new fronters
         # this is for rate limiting and not hitting the pluralkit api too hard
         updateInterval = 5
         if ( time.localtime()[4] % updateInterval ) == 0:
-            updateNeeded = pktools.pullPeriodic()
+            updateNeeded = fetchState()
 
             # If pullPeriodic returns true update the screen and unset updateNeeded
             if updateNeeded:
                 inky_display.set_image(drawScreen(getFronter()))
                 inky_display.show()
-                
-                # Check if not switched out
-                if len(pktools.lastSwitch["members"]) > 0:
-
-                    # Build and send full message
-                    if pktools.pktsettings["discord"]["full"]["enabled"]:
-                        
-                        index = len(pktools.lastSwitch["members"])
-                        message = "Hi, "
-                    
-                        for id in pktools.lastSwitch["members"]:
-                            index = index - 1
-                            member = pktools.getMember(id)
-                            message = message + member["name"]
-
-                            if member["pronouns"] is not None:
-                                message = message + " ( " + member["pronouns"] + " )"
-                            
-                            message = message + "\nYou last fronted:\n" + str(pktools.rsLastSeen(id))[:-10] + " ago\n" + str(pktools.hsTimeShort(pktools.hsLastSeen(id))) 
-                            
-                            if index != 0:
-                                message = message + "\n---"
-                        
-                        sendMessage(message, "full")
-                    
-                    # Build and send filtered message
-                    if pktools.pktsettings["discord"]["filtered"]["enabled"]:
-                        
-                        index = len(pktools.lastSwitch["members"])
-                        message = "Hi, "
-                        
-                        for id in pktools.lastSwitch["members"]:
-                            index = index - 1
-                            member = pktools.getMember(testPrivacy(id))
-                            message = message + member["name"]
-                            if member["pronouns"] is not None:
-                                message = message + " ( " + member["pronouns"] + " )"
-                            if member["uuid"] in flagGroup["members"]:
-                                message = message + " 🔞"
-                            if index != 0:
-                                message = message + ", "
-                        
-                        sendMessage(message, "filtered")
-                        
                 updateNeeded = False
 
     # do nothing for a while
